bfc9267c10905abb1d150b0305301a1e6710724f
AderynGH <131829887+AderynGH@users.noreply.github.com>
2024-08-26 17:24:03 +0100

    Audio Mixer Set Up and Options Menu Buttons Functioning

--- Assets/Classes/Menu/OptionsMenu.cs
+++ Assets/Classes/Menu/OptionsMenu.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
-public class OptionsMenu : MonoBehaviour
-{
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
     public void SetMasterVolume(float volume) {
+        audioMixer.SetFloat("MasterVolume", volume);
         Debug.Log("Master Volume: " + volume);
     }
 
     public void SetMusicVolume(float volume) {
+        audioMixer.SetFloat("MusicVolume", volume);
         Debug.Log("Music Volume: " + volume);
     }
 
-    public void FullscreenToggle(bool isFullscren) {
-        decimal isFullscreenDecimal = isFullscren ? 1 : 0;
-        Debug.Log("Fullscreen: " + isFullscreenDecimal);
+    public void FullscreenToggle(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Fullscreen: " + isFullscreen);
     }
 
     public void OnExitPress() {
-        Debug.Log("Back to Main Menu");
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("Back to Main Menu");
     }
 }
