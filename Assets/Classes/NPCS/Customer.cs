73c29037e0b4eaf2f403cc887791a34782b8e4b4
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2024-08-27 01:03:11 -0500

    Added hierarchy to customers

--- Assets/Classes/NPCS/Customer.cs
+++ Assets/Classes/NPCS/Customer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Purchaser : Customer
+public class Customer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,10 +15,8 @@ public class Purchaser : Customer
     {
 
     }
-
-    public override void Interact()
+    public virtual void Interact()
     {
-        base.Interact();
-        Debug.Log("Purchaser: I want to buy something from your shop.");
+        Debug.Log("Interacting with a customer.");
     }
 }
--- Assets/Classes/NPCS/Gossiper.cs
+++ Assets/Classes/NPCS/Gossiper.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gossiper : MonoBehaviour
+public class Gossiper : Customer
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public override void Interact()
+    {
+        base.Interact();
+        // Add logic to provide information about monsters
     }
 }
