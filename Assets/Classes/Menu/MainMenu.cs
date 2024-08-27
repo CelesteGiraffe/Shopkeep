a4cb963a3748ba22527e6e0b3e819a4f74e575d2
AderynGH <131829887+AderynGH@users.noreply.github.com>
2024-08-27 07:30:02 +0100

    Character Select Functionality Implemented Ish

--- Assets/Classes/Menu/MainMenu.cs
+++ Assets/Classes/Menu/MainMenu.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MenuScript : MonoBehaviour {
+
+    public UnityEngine.UI.Button CharacterSelectBtn1;
+    public UnityEngine.UI.Button CharacterSelectBtn2;
+    private void Start() {
+        CharacterSelectBtn1.onClick.AddListener(() => SelectCharacter("Character1", 2));
+        CharacterSelectBtn2.onClick.AddListener(() => SelectCharacter("Character2", 2));
+    }
 
     public void NewGame(int sceneID) {
         SceneManager.LoadScene(sceneID);
@@ -17,5 +26,10 @@ public class MenuScript : MonoBehaviour {
 
     public void GoToOptions(int sceneID) {
         SceneManager.LoadScene(sceneID);
+    }
+    
+    public void SelectCharacter(string characterName , int SceneID) {
+        Debug.Log(SceneID + " " + characterName);
+        SceneManager.LoadScene(SceneID);
     }
 }
