2808c7568f2390c270e029feccb7634db89e90cb
AderynGH <131829887+AderynGH@users.noreply.github.com>
2024-08-28 22:53:48 +0100

    DialogueManager Updates, final commit.

--- Assets/Classes/NPCS/Customer.cs
+++ Assets/Classes/NPCS/Customer.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour, IInteractable
 {
-    private bool isInteracting = false; 
     public Dialogue dialogue;
+    public List<string[]> customerDialogueSentences = new List<string[]> ();
+    public List<string[]> gossiperDialogueSentences;
+    public List<string[]> leavingSentences;
 
-    private void Awake()
+    public bool hasChosenDialogue = false;
+    public bool hasRequestFullfilled = false;
+    public int dialogueIndex = 0;
+    public bool isInteracting = false;
+
+private void Awake()
     {
         //get the dialogue from the player by tag
         dialogue = GameObject.FindGameObjectWithTag("Player").GetComponent<Dialogue>();
-
+        customerDialogueSentences.Add(new string[] { "Hello", "Goodbye" });
     }
 
     public virtual void Interact()
     {
         isInteracting = true; 
-        dialogue.StartDialogue(new string[] { "Interacting with a customer.", "Goodbye!" }, EndInteraction);
-
-        
+        if (hasRequestFullfilled) {
+            dialogue.StartDialogue(leavingSentences[0], EndInteraction);
+            return;
+        }
+        else if (hasChosenDialogue) {
+            dialogue.StartDialogue(customerDialogueSentences[dialogueIndex], EndInteraction);
+            return;
+        } else {
+            dialogue.StartDialogue(customerDialogueSentences[GetRandomCustomerDialogueIndex()], EndInteraction);
+        }
     }
 
-    private void EndInteraction()
+    public void EndInteraction()
     {
         isInteracting = false; 
     }
@@ -31,5 +45,17 @@ public class Customer : MonoBehaviour, IInteractable
     {
         Collider2D hit = Physics2D.OverlapCircle(targetPosition, 0.5f);
         return hit != null;
+    }
+
+    public int GetRandomCustomerDialogueIndex() {
+        hasChosenDialogue = true;
+        dialogueIndex = Random.Range(0, customerDialogueSentences.Count);
+        return dialogueIndex;
+    }
+
+    public int GetRandomGossiperDialogueIndex() {
+        hasChosenDialogue = true;
+        dialogueIndex = Random.Range(0, gossiperDialogueSentences.Count);
+        return dialogueIndex;
     }
 }
--- Assets/Classes/NPCS/Gossiper.cs
+++ Assets/Classes/NPCS/Gossiper.cs
@@ -7,7 +7,17 @@ public class Gossiper : Customer
 {
     public override void Interact()
     {
-        base.Interact();
-        dialogue.StartDialogue(new string[] { "I heard that there is a big ghost being all ghosty and stuff at the edge of town!" });
+        isInteracting = true;
+        if (hasRequestFullfilled) {
+            dialogue.StartDialogue(leavingSentences[0], EndInteraction);
+            return;
+        }
+        else if (hasChosenDialogue) {
+            dialogue.StartDialogue(customerDialogueSentences[dialogueIndex], EndInteraction);
+            return;
+        }
+        else {
+            dialogue.StartDialogue(customerDialogueSentences[GetRandomCustomerDialogueIndex()], EndInteraction);
+        }
     }
 }
