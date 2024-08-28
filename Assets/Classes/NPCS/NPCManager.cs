eba4ae7c509d9c215ed0ec0b6812b8f75dd00576
AderynGH <131829887+AderynGH@users.noreply.github.com>
2024-08-28 19:02:36 +0100

    Spawning System Functionality Imporvement

--- Assets/Classes/NPCS/NPCManager.cs
+++ Assets/Classes/NPCS/NPCManager.cs
@@ -29,10 +29,26 @@ public class NPCManager : MonoBehaviour
     }
 
     void CheckForNPCSpawn() {
+        Vector3 L_SpawnVariance;
         foreach (var npc in npcDictionary) {
             if (ShouldSpawnNPC(npc.Value)) {
                 if (Random.value <= npc.Value.spawnChance) {
-                    SpawnNPC(npc.Value);
+                    L_SpawnVariance = npc.Value.spawnPosition;
+                    int maxAttempts = 10;
+                    int attempts = 0;
+
+                    while (IsSpawnPositionBlocked(L_SpawnVariance) && attempts < maxAttempts) {
+                        Debug.Log("Spawn position blocked, trying new position");
+                        L_SpawnVariance.x = npc.Value.spawnPosition.x + Random.Range(-2, 2);
+                        L_SpawnVariance.y = npc.Value.spawnPosition.y + Random.Range(0, 2);
+                        attempts++;
+                    }
+                    if (attempts < maxAttempts) {
+                        SpawnNPC(npc.Value, L_SpawnVariance);
+                    }
+                    else {
+                        Debug.Log("Failed to spawn NPC after 10 attempts");
+                    }
                 }
             }
         }
@@ -55,9 +71,21 @@ public class NPCManager : MonoBehaviour
         return false;
     }
 
-    void SpawnNPC(NPCData npcData) {
+    private bool IsSpawnPositionBlocked(Vector3 spawnPosition) {
+        float radius = 0.5f;
+        LayerMask layerMask = LayerMask.GetMask("Obstacle");
+
+        bool isBlocked = Physics2D.OverlapCircle(spawnPosition, radius, layerMask);
+        Debug.Log($"Checking spawn position {spawnPosition} for obstacles. Is blocked: {isBlocked}");
+        return isBlocked;
+    }
+
+    void SpawnNPC(NPCData npcData, Vector3 spawnVariance) {
         int[] currentTime = timeSystem.GetTime();
-        Instantiate(npcData.npcPrefab, npcData.spawnPosition, Quaternion.identity);
+        if (spawnVariance == null) {
+            spawnVariance = npcData.spawnPosition;
+        }
+        Instantiate(npcData.npcPrefab, spawnVariance, Quaternion.identity);
         spawnedNPCList.Add(npcData);
         Debug.Log($"Current Time: {currentTime[2]}:{currentTime[1]}:{currentTime[0]} {currentTime[3]}/{currentTime[4]}/{currentTime[5]}");
     }
