3f723bc6456eed0d9b9b1d812a9a4bc2f403c318
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2024-08-28 14:32:35 -0500

    Money is being shown and edited properly

--- Assets/Classes/Main Character/PlayerManager.cs
+++ Assets/Classes/Main Character/PlayerManager.cs
@@ -1,12 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public int money = 100; 
+    public int money = 100;
+    TextMeshProUGUI goldText;
 
     public void AddMoney(int amount)
     {
         money += amount;
+        SetMoneyText(goldText);
+    }
+    private void Awake()
+    {
+        //find the game object named Gold
+        goldText = GameObject.Find("Gold").GetComponent<TextMeshProUGUI>();
+        SetMoneyText(goldText);
     }
 
     public bool SubtractMoney(int amount)
@@ -14,8 +23,15 @@ public class PlayerManager : MonoBehaviour
         if (money >= amount)
         {
             money -= amount;
+            SetMoneyText(goldText);
             return true;
         }
         return false;
+    }
+
+    //set the text of a text mesh pro object with the money value
+    public void SetMoneyText(TextMeshProUGUI moneyText)
+    {
+        moneyText.text = "£ " + money.ToString();
     }
 }
